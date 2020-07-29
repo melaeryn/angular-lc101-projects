ad1e0d6293c86b0fc15ecac603770e6dc03f4cf1
Marlaina <mlwunder@mtu.edu>
2020-07-29 13:44:14 -0500

    finished input practice

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,10 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle: string) {
+      if(newTitle != "" && !this.movies.includes(newTitle)){
+      this.movies.push(newTitle);
+      }
+   }
 }
